46dde280a3103bdb31bb36268512f239ea1143db
Eddy Lane <naedin@gmail.com>
2018-03-01 12:04:53 +0000

    Cleanup

--- lib/fast_json_diff_ex.ex
+++ lib/fast_json_diff_ex.ex
@@ -1,62 +1,75 @@
 defmodule FastJsonDiffEx do
   def generate(old, new, patches \\ [], path \\ "") do
+    {deleted, patches, old_keys} = patches_for_old(old, new, patches, path)
     new_keys = list_or_map_keys_or_indexes(new)
 
-    old_keys =
-      list_or_map_keys_or_indexes(old)
-      |> Enum.reverse()
-
-    {deleted, patches} =
-      Enum.reduce(old_keys, {false, patches}, fn key, {deleted, patches} ->
-        old_val = item(old, key)
-
-        if has_key_or_index?(new, key) do
-          new_val = item(new, key)
-
-          if map_or_list?(old_val) and map_or_list?(new_val) do
-            child_patches =
-              generate(
-                old_val,
-                new_val,
-                [],
-                path <> "/" <> escape_path_component(key)
-              )
-
-            {deleted || false, patches ++ child_patches}
-          else
-            if new_val !== old_val do
-              {deleted || false, patches ++ [patch(:replace, path, key, new_val)]}
-            else
-              {deleted || false, patches}
-            end
-          end
-        else
-          {true, patches ++ [patch(:remove, path, key)]}
-        end
-      end)
-
-    if !deleted and length(new_keys) == length(old_keys) do
+    unless deleted or length(new_keys) != length(old_keys) do
       patches
     end
 
     Enum.reduce(new_keys, patches, fn key, patches ->
       if !has_key_or_index?(old, key) do
-        patches ++ [patch(:add, path, key, item(new, key))]
+        add_patch(patches, patch(:add, path, key, item(new, key)))
       else
         patches
       end
     end)
   end
 
+  defp patches_for_old(old, new, patches, path) do
+    old_keys =
+      list_or_map_keys_or_indexes(old)
+      |> Enum.reverse()
+
+    {deleted, patches} = Enum.reduce(old_keys, {false, patches}, old_key_reducer(old, new, path))
+
+    {deleted, patches, old_keys}
+  end
+
+  defp old_key_reducer(old, new, path) do
+    fn key, {deleted, patches} ->
+      old_val = item(old, key)
+
+      case has_key_or_index?(new, key) do
+        true ->
+          new_val = item(new, key)
+
+          cond do
+            map_or_list?(old_val) and map_or_list?(new_val) ->
+              child_patches =
+                generate(
+                  old_val,
+                  new_val,
+                  [],
+                  path_component(path, key)
+                )
+
+              {deleted || false, add_patch(patches, child_patches)}
+
+            new_val === old_val ->
+              {deleted || false, patches}
+
+            true ->
+              # Replace value
+              {deleted || false, add_patch(patches, patch(:replace, path, key, new_val))}
+          end
+
+        false ->
+          # Remove value
+          {true, add_patch(patches, patch(:remove, path, key))}
+      end
+    end
+  end
+
+  defp add_patch(patches, new_patches) when is_list(new_patches), do: patches ++ new_patches
+  defp add_patch(patches, new_patch), do: patches ++ [new_patch]
+
   defp has_key_or_index?(map, key) when is_map(map) and is_binary(key) do
     Map.has_key?(map, key)
   end
 
   defp has_key_or_index?(list, index) when is_list(list) and is_integer(index) do
-    case Enum.at(list, index) do
-      nil -> false
-      _ -> true
-    end
+    Enum.at(list, index) != nil
   end
 
   defp has_key_or_index?(_, _), do: false
@@ -64,7 +77,7 @@ defmodule FastJsonDiffEx do
   defp patch(:add, path, key, val) do
     %{
       "op" => "add",
-      "path" => path <> "/" <> escape_path_component(key),
+      "path" => path_component(path, key),
       "value" => val
     }
   end
@@ -72,7 +85,7 @@ defmodule FastJsonDiffEx do
   defp patch(:replace, path, key, val) do
     %{
       "op" => "replace",
-      "path" => path <> "/" <> escape_path_component(key),
+      "path" => path_component(path, key),
       "value" => val
     }
   end
@@ -80,9 +93,11 @@ defmodule FastJsonDiffEx do
   defp patch(:remove, path, key) do
     %{
       "op" => "remove",
-      "path" => path <> "/" <> escape_path_component(key)
+      "path" => path_component(path, key)
     }
   end
+
+  defp path_component(path, key), do: path <> "/" <> escape_path_component(key)
 
   defp list_or_map_keys_or_indexes(map) when is_map(map), do: Map.keys(map)
 
@@ -112,29 +127,5 @@ defmodule FastJsonDiffEx do
         path = Regex.replace(~r/~/, path, "~0")
         Regex.replace(~r/\//, path, "~1")
     end
-  end
-
-  def test() do
-    a = %{
-      "firstName" => "Albert",
-      "lastName" => "Einstein",
-      "phoneNumbers" => [
-        %{"number" => "12345"},
-        %{"number" => "45353"},
-        %{"number" => "67891"}
-      ]
-    }
-
-    b = %{
-      "firstName" => "Joachim",
-      "lastName" => "Wester",
-      "phoneNumbers" => [
-        %{"number" => "123"},
-        %{"number" => "456"},
-        %{"number" => "678"}
-      ]
-    }
-
-    generate(a, b)
   end
 end
